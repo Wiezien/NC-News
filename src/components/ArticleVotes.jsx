bf7d334fb3ae0c750e61bcaae4249af171ef957a
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-05-08 15:49:08 +0100

    ticket 7 complete

---
+++ src/components/ArticleVotes.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { patchVotesByArticleId } from "../api";
+
+function ArticleVotes({ article, currentVotes }) {
+    
+  const [votes, setVotes] = useState(0);
+  const [error, setError] = useState(null);
+  const [optimisticMsg, setOptimisitcMsg] = useState(null);
+
+  const article_id = article.article_id;
+
+  const handleVote = (vote) => {
+    setVotes(votes + vote);
+    patchVotesByArticleId(article_id, vote)
+      .then(() => {
+        setOptimisitcMsg("Vote Submitted!");
+      })
+      .catch((error) => {
+        setError("Unable to update votes. Please try again.");
+      });
+  };
+
+  return (
+    <div id="article-vote">
+      <button className="vote-button" disabled={votes === 1} onClick={() => handleVote(1)}>
+        +
+      </button>
+      {article.votes + votes}
+      <button className="vote-button" disabled={votes === -1} onClick={() => handleVote(-1)}>
+        -
+      </button>
+      {optimisticMsg && <p>{optimisticMsg}</p>}
+    </div>
+  );
+}
+
+export default ArticleVotes;
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+
 function CommentCard({ comments }) {
   return (
     <section>
       <h2>Comments:</h2>
+
       {comments.map((comment) => (
-        <p key={comment.id} id="comment-item">
-          {comment.body}
-        </p>
+        <li key={comment.comment_id}>
+          <h3>By {comment.author}</h3>
+          <h4>Votes {comment.votes}</h4>
+          <h4>{comment.created_at}</h4>
+          <p id="comment-item">{comment.body}</p>
+        </li>
       ))}
     </section>
   );
